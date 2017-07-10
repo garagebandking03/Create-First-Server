e9142ca3bb53aeb4e902f130add3158a72d8ec54
Luke Evers <garagebandking03@gmail.com>
2017-07-10 14:55:28 -0700

    Create more complexity
    Add url checking with if else clauses

--- http.js
+++ http.js
@@ -1,11 +1,29 @@
+// var http = require("http");
+// http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'application/javascript'});
+//    res.end(JSON.stringify({ hello: 'world' }));
+// }).listen(8081);
+// console.log('Server running at http://127.0.0.1:8081/');
+
 const createServer = require('http').createServer;
 
 const PORT = process.env.PORT || 3000;
 
 const app = createServer(
   (req, res) => {
-    console.log(req.headers);
-    res.end('AAAAHHHHHHHHH!!!!!!!!!!');
+    const {
+      url,
+      headers,
+      method,
+    } = req;
+    console.log(url, headers,method);
+    if(url === '/') {
+      return;
+    } else if(url ==== '/fuckoff') {
+      return;
+    }
+    res.writeHead(404, { 'Content-Type': 'text/plain'});
+    res.end('Not Found');
   }
 ).listen(
   PORT,
